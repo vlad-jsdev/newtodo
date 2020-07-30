65cc0978f02be749b8334b8ee74d1e342f100f4a
vladosxxx <vladosxxx@gmail.com>
2020-07-30 12:21:03 +0300

    wrote done tasks

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,26 +1,38 @@
 <template>
     <div>
-        <v-text-field label="New Task" hide-details="auto" v-model=task v-on:keyup.enter="AddNewTodo"></v-text-field>
-    <v-list>{{ todo }}</v-list>
-    <v-btn v-on:click="AddNewTodo(task)">Add</v-btn>
+        <v-container>
+            <v-row>
+                <v-col>
+                    <v-text-field label="New Task" hide-details="auto" v-model=task v-on:keyup.enter="AddNewTodo"></v-text-field>
+                    <v-btn v-on:click="AddNewTodo(task)">Add</v-btn>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col>
+                    <v-list v-for="(todo, id) in todos" v-bind:key="id">{{ todo.textTodo }}<v-btn v-on:click="DoneTodo(todos.indexOf(todo))">Done</v-btn></v-list>
+                </v-col></v-row>
+                {{ doneTasks }}
+        </v-container>
     </div>
 </template>
 <script>
     export default {
         name: 'Todo',
         data: () => ({
-                todo: [
-                    {id: Number, textTodo: String, date: String, done: Boolean}
-                    ],
-                task: ''
+                todos: [],
+                task: '',
+                doneTasks: []
         }),
         methods:{
             AddNewTodo() {
-                this.todo.push({id: new Date(), textTodo: this.task, date: new Date(), done: false})
+                if(this.task === '')
+                    return
+                this.todos.push({id: new Date(), textTodo: this.task, date: new Date(), alarm: ''})
                 this.task = ''
             },
-            DelTodo(){
-                console.log('Remove')
+            DoneTodo(a){
+                this.doneTasks.push(this.todos[a])
+                this.todos.splice(a,1)
             }
         }
 
