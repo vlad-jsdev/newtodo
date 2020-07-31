f056cf81610ba6d564647139e07c3209636038a7
vladosxxx <vladosxxx@gmail.com>
2020-07-31 17:51:04 +0300

    added theme

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,23 @@
             clipped
     >
       <v-list dense>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-view-dashboard</v-icon>
+        <v-list-item @click.stop="main = true, donePage = false; settings = false">
+          <v-list-item-action >
+            <v-icon>mdi-notebook-edit</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Dashboard</v-list-item-title>
+            <v-list-item-title>Todo Tasks</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-        <v-list-item link>
+        <v-list-item @click.stop="donePage = true; main = false; settings = false">
+          <v-list-item-action >
+            <v-icon>mdi-notebook-check</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Done Tasks</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item @click.stop="settings = true; donePage = false; main = false">
           <v-list-item-action>
             <v-icon>mdi-cog</v-icon>
           </v-list-item-action>
@@ -35,7 +43,6 @@
 
     <v-main>
       <v-container
-              class="fill-height"
               fluid
       >
         <v-row
@@ -43,13 +50,25 @@
                 justify="center"
         >
           <v-col>
+
+            <div v-if="main">
                 <Todo v-on:done="getDone($event)"/>
-                <Done :doneTasks="doneTasks"/>
+            </div>
+            <div v-else-if="donePage">
+              <Done :doneTasks="doneTasks"/>
+            </div>
+              <div v-else-if="settings">
+                <H3>Theme</H3>
+                <div class="d-flex justify-center align-center"><h4 class="mx-2">Light</h4><v-switch v-model="$vuetify.theme.dark" :label="Theme" ></v-switch><h4 class="mx-2">Dark</h4></div>
+
+              </div>
+            <div v-else>
+              <Todo v-on:done="getDone($event)"/>
+            </div>
           </v-col>
         </v-row>
       </v-container>
     </v-main>
-
     <v-footer app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
@@ -68,7 +87,10 @@
     data: () => {
       return ({
         doneTasks: [],
-        drawer: null,
+        main: null,
+        donePage: null,
+        settings: null,
+        drawer: null
       });
     },
     methods:{
@@ -76,8 +98,14 @@
             this.doneTasks = x
         }
     },
+    computed:{
+
+    },
     created () {
       this.$vuetify.theme.dark = true
     },
   }
 </script>
+<style>
+
+</style>
