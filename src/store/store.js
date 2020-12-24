5567e04804c49fcce4d19f49550f40c78944e034
vladosxxx <vladosxxx@gmail.com>
2020-12-24 13:32:13 +0200

    build into doc

--- src/store/store.js
+++ src/store/store.js
@@ -10,57 +10,48 @@ export default new Vuex.Store({
     },
 
     getters: {
-        // Here we will create a getter
-        // sortByIdStore: state => {
-        //     function compare (a, b) {
-        //         if ( a.id > b.id ){
-        //             return -1;
-        //         }
-        //         if ( a.id < b.id ){
-        //             return 1;
-        //         }
-        //         return 0;
-        //     }
-        //     return state.todos.sort(compare)
-        // }
+
     },
 
     mutations: {
-        startLocal(state){
+        startLocal(state) {
             if (localStorage.getItem('todos') || localStorage.getItem('doneTasks')) {
                 try {
                     state.todos = JSON.parse(localStorage.getItem('todos'))
                     state.doneTasks = JSON.parse(localStorage.getItem('doneTasks'))
-                } catch(e) {
+                } catch (e) {
                     localStorage.removeItem('todos')
                     localStorage.removeItem('doneTasks')
                 }
             }
+
+            // fetch('https://jsonplaceholder.typicode.com/todos')
+            //     .then(response => response.json())
+            //     .then(data => state.todos = data)
         },
-        addTodo(state, item){
-            state.todos.unshift({id: new Date(), textTodo: item.task, date: item.date, time: item.time}) //push todos to storage
+        addTodo(state, item) {
+            state.todos.unshift({ id: new Date(), textTodo: item.task, date: item.date, time: item.time }) //push todos to storage
             localStorage.setItem('todos', JSON.stringify(state.todos)) // Write to localStorage todos
         },
-        doneTodo(state, i){
+        doneTodo(state, i) {
             state.doneTasks.push(state.todos[i])
             state.todos.splice(i, 1)
             localStorage.setItem('doneTasks', JSON.stringify(state.doneTasks))
             localStorage.setItem('todos', JSON.stringify(state.todos))
         },
-        delTodo(state, i){
+        delTodo(state, i) {
             state.doneTasks.splice(i, 1)
             localStorage.setItem('doneTasks', JSON.stringify(state.doneTasks))
         },
-        editTodo(state, i){
-            state.todos.splice(i.i, 1, {id: i.id, textTodo: i.task, date: i.date, time: i.time})
+        editTodo(state, i) {
+            state.todos.splice(i.i, 1, { id: i.id, textTodo: i.task, date: i.date, time: i.time })
             localStorage.setItem('todos', JSON.stringify(state.todos))
         },
-        saveTodo(state, i){
+        saveTodo(state, i) {
             state.todos = i
             localStorage.setItem('todos', JSON.stringify(state.todos))
         }
     },
 
-    actions: {
-    }
+    actions: {}
 });
